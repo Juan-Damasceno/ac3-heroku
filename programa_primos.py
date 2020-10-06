bdb610ef320affc830e195a522545a3fd352d42c
Juan-Damasceno <juan.sceno@outlook.com>
2020-10-05 21:31:50 -0300

    AC03

---
+++ programa_primos.py
@@ -0,0 +1,27 @@
+import os
+from flask import Flask, jsonify, request
+from math import sqrt
+
+app = Flask(__name__)
+
+@app.route('/')
+def nao_entre_em_panico():
+
+    primos = "2, "
+    lista = []
+    acumulador = 0
+    numero = 3
+
+    while len(lista) < 99:
+        for i in range(1, (numero+1)):
+            if numero % i == 0:
+                acumulador += 1
+        if acumulador == 2:
+            primos = primos + str(numero) + ", "
+            lista.append(numero)
+        acumulador = 0
+        numero += 1
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
